46daac317047e79bb9fdc4e34dcde2e63b78d217
PARK-SHIN <1016ps@naver.com>
2022-01-08 17:59:37 +0900

    test

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require('body-parser');
+// User 가져오기
+const { User } = require("./models/User");
+
+// application/s-www-form-urlencoded를 분석해서 가져온다.
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json 타입을 분석해서 가져온다.
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 /*
@@ -16,11 +25,28 @@ mongoose
 
 
 // root디렉토리 오게되면 Hello World! 출력
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => res.send('Hello World!!!'))
+
+
+// 클라인언트에서 보내는 정보
+app.post('/register', (req, res) => {
+
+  // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+      // 정보를 db에 넣기 위해서는 req.body(request.body) 사용
+      // bodyParser가 있기에 가능
+      const user = new User(req.body)
+
+      user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({     // status=200 은 성공했다.
+          success: true
+        })
+      })
 })
 
+
+
 // port 5000 실행
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}!`)
-})
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}!`))
